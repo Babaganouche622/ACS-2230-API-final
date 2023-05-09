a100742fabd77ddbe086b039c9a76717aa58f2e0
Brian <briangordon.cahill@students.dominican.edu>
2023-05-08 19:19:33 -0700

    Removed a line in server js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const User = require('./models/user');
+// const User = require('./models/user');
 
 require('./controllers/user.js')(app);
 
